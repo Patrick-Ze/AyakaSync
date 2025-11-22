dd41338f301a5165a14500ad7d7f8b66469d165a
Patrick-Ze <19711799+Patrick-Ze@users.noreply.github.com>
2025-11-22 23:08:31 +0800

    Initial version: works for GOOD format

---
+++ README.md
@@ -0,0 +1,25 @@
+使用养成计算器API同步原神背包的物品信息
+
+# 安装&配置
+
+下载此仓库后，修改`config`文件内的示例文件，在其中填写你的米游社Cookie，并另存为`yaml`后缀。
+
+或者，你可以直接通过创建符号链接或者docker共享等方式，直接使用[MihoyoBBSTools](https://github.com/Womsxd/MihoyoBBSTools)的`config`文件夹
+
+# 使用
+
+**API 调用：**
+
+- GOOD格式
+
+  `http://127.0.0.1:20928/package/good/<uid>`
+
+  以curl为例：`curl 'http://127.0.0.1:20928/package/good/123456789' -H 'accept: application/json'`
+
+## 已知局限
+
+受养成计算器API的限制：
+
+- 获取的物品数量与游戏实时数据相比，有大约10分钟的延迟
+- 经验书和精锻用矿只能获取到品质最高的那个的数量
+- 当背包中的某种物品的数量多于常规养成所需时，获取的物品数量会少于实际数量。例如，如果你有17亿摩拉时
---
+++ main.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import shutil
+from glob import glob
+from itertools import cycle, islice
+
+import yaml
+import requests
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+def init_delta_request():
+    with open("metadata/avatar.json", "rt", encoding="utf-8") as f:
+        avatar_data = json.load(f)
+    with open("metadata/weapon.json", "rt", encoding="utf-8") as f:
+        weapon_data = json.load(f)
+
+    avatar_by_type = {}
+    for avatar in avatar_data:
+        wtype = avatar["weapon_type"]
+        avatar_by_type.setdefault(wtype, []).append(avatar)
+    weapon_by_type = {}
+    for weapon in weapon_data:
+        wtype = weapon["weapon_type"]
+        weapon_by_type.setdefault(wtype, []).append(weapon)
+
+    promotion_deltas = []
+    for wtype, characters in avatar_by_type.items():
+        weapons = weapon_by_type.get(wtype, [])
+        max_len = max(len(weapons), len(characters))
+        cycle_weapons = islice(cycle(weapons), max_len)
+        cycle_characters = islice(cycle(characters), max_len)
+        for char, weap in zip(cycle_characters, cycle_weapons):
+            delta = {
+                "avatar_id": char["id"],
+                "avatar_level_current": 1,
+                "avatar_level_target": 90,
+                "skill_list": [{"id": i, "level_current": 1, "level_target": 10} for i in char["skill_list"]],
+                "weapon": {
+                    "id": weap["id"],
+                    "level_current": 1,
+                    "level_target": 90,
+                },
+            }
+            promotion_deltas.append(delta)
+    return promotion_deltas
+
+
+def read_config_files():
+    config_data = {}
+    os.makedirs("config", exist_ok=True)
+    cfg_files = glob("config/*.yaml")
+    if len(cfg_files) == 0:
+        default_file = "config/config.yaml"
+        shutil.copy(f"{default_file}.example", default_file)
+        raise SystemExit(f"Invalid config file, please update `{default_file}`")
+    for path in cfg_files:
+        with open(path, "r+", encoding="utf-8") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            uid = data["games"]["cn"]["genshin"].get("uid")
+            if not uid:
+                params = {"game_biz": "hk4e_cn"}
+                api_url = "https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie"
+                headers = {
+                    "Content-Type": "application/json",
+                    "Cookie": data["account"]["cookie"],
+                    "User-Agent": data["games"]["cn"]["useragent"],
+                }
+                r = requests.get(api_url, params=params, headers=headers)
+                r.raise_for_status()
+                d = r.json()
+                assert d["retcode"] == 0, d["message"]
+                match = next(i for i in d["data"]["list"] if i["game_biz"] == params["game_biz"])
+                uid = match["game_uid"]
+                data["games"]["cn"]["genshin"]["uid"] = uid
+
+                f.seek(0)
+                f.write(yaml.dump(data, Dumper=yaml.Dumper, sort_keys=False))
+                f.truncate()
+                f.flush()
+            uid = str(uid)
+            config_data[uid] = {"cookie": data["account"]["cookie"], "ua": data["games"]["cn"]["useragent"]}
+    return config_data
+
+
+def get_overall_consume(uid: str, region="cn_gf01"):
+    cfg = uid_config_data[uid]
+    payload = {"items": deltas, "region": region, "uid": uid}
+    headers = {
+        "Cookie": cfg["cookie"],
+        "Content-Type": "application/json",
+        "Referer": "https://webstatic.mihoyo.com",
+        "User-Agent": cfg["ua"],
+    }
+    api_url = "https://api-takumi.mihoyo.com/event/e20200928calculate/v3/batch_compute"
+    r = requests.post(api_url, json=payload, headers=headers)
+    r.raise_for_status()
+    data = r.json()
+    if data.get("retcode") != 0:
+        raise Exception(f"API Error {data.get('retcode')}: {data.get('message')}")
+
+    # 提取背包数据
+    overall_consume = data.get("data", {}).get("overall_consume", [])
+    return overall_consume
+
+
+GOOD_id_map = {}
+deltas = init_delta_request()
+uid_config_data = read_config_files()
+
+
+# 初始化 FastAPI 应用
+app = FastAPI()
+
+
+class User(BaseModel):
+    uid: str
+
+
+@app.get("/package/raw/{uid}", summary="根据UID获取用户背包内容")
+async def read_user(uid: str):
+    if uid not in uid_config_data:
+        raise HTTPException(status_code=404, detail=f"User with uid {uid} not found in the known list")
+    inventories = get_overall_consume(uid)
+    return inventories
+
+
+@app.get("/package/good/{uid}", summary="根据UID获取用户背包内容 (返回GOOD格式)")
+async def read_user(uid: str):
+    global GOOD_id_map
+    if uid not in uid_config_data:
+        raise HTTPException(status_code=404, detail=f"User with uid {uid} not found in the known list")
+    overall_consume = get_overall_consume(uid)
+
+    inventory = []
+    for item in overall_consume:
+        total_num = item["num"]
+        lack_num = item["lack_num"]
+        actual_count = total_num - lack_num  # 实际拥有数量
+        if actual_count > 0:
+            inventory.append({"id": item["id"], "count": actual_count, "accurate": lack_num != 0})
+
+    if len(GOOD_id_map) == 0:
+        with open("metadata/MaterialExcelConfigData_idmap_gen.json", "rt", encoding="utf-8") as f:
+            GOOD_id_map = json.load(f)
+            GOOD_id_map = {int(k): v for k, v in GOOD_id_map.items()}
+    materials = {GOOD_id_map[i["id"]]: i["count"] for i in inventory}
+    result = {
+        "format": "GOOD",
+        "version": 3,
+        "source": "https://github.com/Patrick-Ze/AyakaSync",
+        "materials": materials,
+    }
+    return result
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(cwd)
+
+    filename = os.path.splitext(os.path.basename(__file__))[0]
+    uvicorn.run(
+        f"{filename}:app",
+        port=20928,
+        host="0.0.0.0",
+        reload=True,
+    )
